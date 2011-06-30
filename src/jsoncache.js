f741f2ba8126044c86a735a66968482f469ae820
Kimmo Puputti <kpuputti@gmail.com>
2011-06-30 10:51:55 +0300

    Added doc for the compiler and requirements check.

--- src/jsoncache.js
+++ src/jsoncache.js
@@ -1,3 +1,11 @@
+/**
+ * @preserve JSONCache version #VERSION#
+ *
+ * Author: Kimmo Puputti (kpuputti at gmail)
+ *
+ * See README.rst at https://github.com/kpuputti/JSONCache
+ * for requirements and usage.
+ */
 /*jslint white: true, devel: true, onevar: false, undef: true, nomen: true,
   regexp: true, plusplus: false, bitwise: true, newcap: true, maxerr: 50,
   indent: 4 */
@@ -7,26 +15,46 @@
 
 
     // Configuration.
-    var config = {
+    var settings = {
+
+        // This is expanded by the build process from the VERSION.txt file.
+        version: '#VERSION#',
+
         debug: true,
-        prefix: 'JSONCache.'
+        prefix: 'JSONCache',
+        keySeparator: ' '
     };
 
     var log = function () {
-        if (config.debug && window.console) {
+        if (settings.debug && window.console) {
             var args = Array.prototype.slice.call(arguments);
             args.unshift('JSONCache:');
             console.log.apply(console, args);
         }
     };
 
+    // Check the prerequisites for the browser.
+    settings.browserOk = (function () {
+        var jsonOk = window.JSON &&
+            typeof window.JSON.parse === 'function' &&
+            typeof window.JSON.stringify === 'function';
+        var localStorageOk;
+        try {
+            localStorageOk =  'localStorage' in window && window.localStorage !== null;
+        } catch (e) {
+            localStorageOk = false;
+        }
+        return jsonOk && localStorageOk;
+    }());
+
     // Namespace for all the code.
     var JSONCache = {};
+    JSONCache.settings = settings;
 
     JSONCache.getCachedJSON = function (options) {
         var url = options.url;
         var success = options.success;
-        var key = config.prefix + options.url;
+        var key = settings.prefix + settings.keySeparator + options.url;
         var cachedData = window.localStorage[key];
         if (cachedData) {
             log('Value found from cache for url:', url);
