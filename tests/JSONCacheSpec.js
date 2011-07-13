acbe9c1238bc9040ab5d5d4ace7c1416ff94c3c5
Kimmo Puputti <kpuputti@gmail.com>
2011-07-13 17:31:51 +0300

    Migrated more tests.

--- tests/JSONCacheSpec.js
+++ tests/JSONCacheSpec.js
@@ -1,10 +1,30 @@
 /*jslint white: true, devel: true, onevar: false, undef: true, nomen: false,
   regexp: true, plusplus: false, bitwise: true, newcap: true, maxerr: 50,
   indent: 4 */
-/*global describe: false, it: false, expect: false,
-  jQuery: false, JSONCache: false, window: false */
+/*global beforeEach: false, describe: false, it: false, expect: false,
+  spyOn: false, jQuery: false, JSONCache: false, window: false,
+  waitsFor: false, runs: false */
 
 describe('JSONCache Test Suite.', function () {
+
+    var testData;
+
+    beforeEach(function () {
+        try {
+            window.localStorage.clear();
+        } catch (e) {
+            // Fail silently if localStorage is not supported.
+            // Support is checked in the first test case.
+        }
+        testData = {
+            "success": true,
+            "data": [
+                "först item",
+                "secönd itém"
+            ],
+            "Weird väl": 666
+        };
+    });
 
     it('should satisfy the requirements', function () {
 
@@ -15,6 +35,7 @@ describe('JSONCache Test Suite.', function () {
         expect(typeof JSON.stringify).toBe('function');
         expect(window.localStorage).toBeDefined();
         expect(window.localStorage).not.toBeNull();
+        expect(window.localStorage.length).toBe(0);
 
         // Libraries.
         expect(typeof jQuery).toBe('function');
@@ -31,7 +52,51 @@ describe('JSONCache Test Suite.', function () {
         expect(JSONCache.settings.numTries).toBe(5);
         expect(JSONCache.settings.waitTime).toBe(200);
         expect(JSONCache.settings.itemLifetime).toBe(5 * 60 * 1000);
+    });
 
+    it('should have the basic localStorage functionality', function () {
+        expect(window.localStorage.length).toBe(0);
+        window.localStorage['söme key'] = 'söme dätä';
+        expect(window.localStorage.length).toBe(1);
+        expect(window.localStorage['söme key']).toBe('söme dätä');
+    });
+
+    it('should return correct data and cache it', function () {
+
+        // Mock the proxy and time functions.
+        spyOn(JSONCache, '_getJSONProxy').andCallFake(function (url, options) {
+            options.success(testData);
+        });
+        spyOn(JSONCache, '_getTime').andReturn(2345678901234);
+
+        var done = false;
+        var returnedData;
+        JSONCache.getCachedJSON('data.json', {
+            success: function (data) {
+                returnedData = data;
+                done = true;
+            }
+        });
+
+        waitsFor(function () {
+            return done;
+        }, 'Ajax success function', 10000);
+        runs(function () {
+            expect(returnedData).toEqual({
+                success: true,
+                data: [
+                    'först item',
+                    'secönd itém'
+                ],
+                'Weird väl': 666
+            });
+            expect(returnedData).toEqual(JSON.parse(window.localStorage['JSONCache data data.json']));
+            expect(window.localStorage['JSONCache time data.json']).toBe('2345678901234');
+            expect(JSONCache._getJSONProxy.callCount).toBe(1);
+            expect(JSONCache._getJSONProxy.mostRecentCall.args[0]).toBe('data.json');
+            expect(JSONCache._getTime.callCount).toBe(1);
+            expect(window.localStorage.length).toBe(2);
+        });
     });
 
 });
